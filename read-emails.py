2fc588a55c01e2c495716a3ca378bd8b9051769f
Martin <martin.evenbom@gmail.com>
2024-08-30 12:00:43 +0200

    script done

---
+++ read-emails.py
@@ -0,0 +1,44 @@
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow, Request
+from googleapiclient.discovery import build
+import os
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    results = service.users().messages().list(userId='me').execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        print('No messages found.')
+    else:
+        print('Message IDs:')
+        for message in messages:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            print(msg['snippet'])  # Print email snippet
+
+if __name__ == '__main__':
+    main()
+
